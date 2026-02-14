bbaec11e1dfe066986c6aac87ee6d234a0f55827
Abdul Wahid <abdulwahid211@gmail.com>
2026-02-14 11:10:52 +0000

    example

---
+++ LogTracker/LogTracker/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static string folderPath = @"/Users/abdulwahid/Downloads";
+    public static string fileName = "health.txt";
+    
+    private static bool _disposed = false;
+    
+    
+    static async Task Main()
+    {
+    
+        using var timer = new PeriodicTimer(TimeSpan.FromSeconds(2));
+       
+        Console.WriteLine("Started...");
+       
+       while (!_disposed)
+       {
+           while (await timer.WaitForNextTickAsync())
+           {
+               CheckEnvironmentWorkerHealth();
+               if (_disposed)
+                   break;
+           }
+       }
+       
+       Console.WriteLine("Stopped...");
+   
+    }
+
+    public static void CheckEnvironmentWorkerHealth()
+    {
+        Console.WriteLine($"Checking at {DateTime.Now}");
+        string fullPath = Path.Combine(folderPath, fileName);
+
+        if (File.Exists(fullPath))
+        {
+            string content = File.ReadAllText(fullPath);
+
+            if (content.Contains("Error") || content.Contains("Failed"))
+            {
+                Console.WriteLine($"{DateTime.Now}:Not Healthy");
+                _disposed = true;
+            }
+
+            Console.WriteLine(content);
+        }
+    }
+}
